a189df0abfeda831c476e5c4ef17f06c6ad2207f
Kimberly McKinnon <kimberlite17@gmail.com>
2021-04-07 18:34:06 -0600

    mock data for books

--- mock-data.js
+++ mock-data.js
@@ -1,358 +1,403 @@
 let mock = [{
-    id: 1,
-    name: "Egg Salad",
-    price: "$5.62",
-    country: "United States",
-    image: 'egg-salad.jpg'
-  },
-  {
-    id: 2,
-    name: "Sweet Potato",
-    price: "$2.41",
-    country: "Brazil",
-    image: "sweet-potato.jpg"
-  },
-  {
-    id: 3,
-    name: "Foccacia Bread",
-    price: "$5.92",
-    country: "United States",
-    image: 'focaccia.jpg'
-  },
-  {
-    id: 4,
-    name: "Canned Lotus Rootlets",
-    price: "$3.58",
-    country: "United States",
-    image: 'lotus-rootlets.jpg'
-  },
-  {
-    id: 5,
-    name: "Frilled Toothpicks",
-    price: "$2.47",
-    country: "Canada",
-    image: 'toothpicks.jpg'
-
-  },
-  {
-    id: 6,
-    name: "Rubbed Sage",
-    price: "$8.77",
-    country: "Brazil",
-    image: 'sage-rubbed.jpg'
-  },
-  {
-    id: 7,
-    name: "Langers Mango Nectar",
-    price: "$9.24",
-    country: "Brazil",
-    image: 'mango-nectar.jpg'
-  },
-  {
-    id: 8,
-    name: "Cucumber",
-    price: "$3.02",
-    country: "Canada",
-    image: 'cucumber.jpg'
-  },
-  {
-    id: 9,
-    name: "Fava Beans",
-    price: "$4.76",
-    country: "Brazil",
-    image: 'fava-beans.jpg'
-
-  },
-  {
-    id: 10,
-    name: "Coconut",
-    price: "$2.50",
-    country: "Brazil",
-    image: 'coconut.jpg'
-  },
-  {
-    id: 11,
-    name: "Kettle Potato Chips",
-    price: "$6.70",
-    country: "United States",
-    image: "potato-chips.jpg"
-  },
-  {
-    id: 12,
-    name: "Flour - Rye",
-    price: "$1.14",
-    country: "Canada",
-    image: "rye-flour.jpg"
-  },
-  {
-    id: 13,
-    name: "Feta Cheese",
-    price: "$6.92",
-    country: "Brazil",
-    image: "feta-cheese.jpg"
-  },
-  {
-    id: 14,
-    name: "Cheddar Cheese",
-    price: "$5.36",
-    country: "Mexico",
-    image: "cheddar-cheese.jpg"
-  },
-  {
-    id: 15,
-    name: "Capers",
-    price: "$4.18",
-    country: "United States",
-    image: "capers.jpg"
-  },
-  {
-    id: 16,
-    name: "Baguette",
-    price: "$9.18",
-    country: "Mexico",
-    image: "baguette.jpg"
-  },
-  {
-    id: 17,
-    name: "Lasagna Noodles",
-    price: "$4.73",
-    country: "Brazil",
-    image: "lasagna-pasta.jpg"
-  },
-  {
-    id: 18,
-    name: "Pied De Vents Cheese",
-    price: "$5.16",
-    country: "Canada",
-    image: "pied-de-vents.jpg"
-  },
-  {
-    id: 19,
-    name: "Sweet Potato Crackers",
-    price: "$6.81",
-    country: "United States",
-    image: "crackers.jpg"
-  },
-  {
-    id: 20,
-    name: "Mushrooms",
-    price: "$8.62",
-    country: "United States",
-    image: "mushrooms.jpg"
-  },
-  {
-    id: 21,
-    name: "Jerusalem Artichoke",
-    price: "$3.16",
-    country: "United States",
-    image: "jerusalem-artichoke.jpg"
-  },
-  {
-    id: 22,
-    name: "Leather Leaf Fern",
-    price: "$9.41",
-    country: "Brazil",
-    image: "leather-leaf.jpg"
-  },
-  {
-    id: 23,
-    name: "Dried Apricots",
-    price: "$7.42",
-    country: "Brazil",
-    image: "dried-apricots.jpg"
-  },
-  {
-    id: 24,
-    name: "Navel Orange",
-    price: "$3.53",
-    country: "Brazil",
-    image: "navel-orange.jpg"
-  },
-  {
-    id: 25,
-    name: "Sunflower Oil",
-    price: "$1.90",
-    country: "Mexico",
-    image: "sunflower-oil.jpg"
-  },
-  {
-    id: 26,
-    name: "Black Turtle Beans",
-    price: "$4.67",
-    country: "Canada",
-    image: "turtle-beans.jpg"
-  },
-  {
-    id: 27,
-    name: "Bananas",
-    price: "$2.64",
-    country: "Canada",
-    image: "bananas.jpg"
-
-  },
-  {
-    id: 28,
-    name: "Salami",
-    price: "$8.72",
-    country: "Brazil",
-    image: "salami.jpg"
-  },
-  {
-    id: 29,
-    name: "Proscuitto",
-    price: "$9.86",
-    country: "Brazil",
-    image: "proscuitto.jpg"
-  },
-  {
-    id: 30,
-    name: "Cranberries",
-    price: "$5.85",
-    country: "United States",
-    image: "cranberries.jpg"
-
-  },
-  {
-    id: 31,
-    name: "Canteloupe",
-    price: "$4.49",
-    country: "United States",
-    image: "canteloupe.jpg"
-  },
-  {
-    id: 32,
-    name: "Lemon",
-    price: "$2.8Lemon5",
-    country: "United States",
-    image: "lemon.jpg"
-  },
-  {
-    id: 33,
-    name: "Pesto",
-    price: "$2.67",
-    country: "Brazil",
-    image: "pesto.jpg"
-  },
-  {
-    id: 34,
-    name: "Grapefruit Juice",
-    price: "$4.88",
-    country: "Brazil",
-    image: "grapefruit-juice.jpg"
-
-  },
-  {
-    id: 35,
-    name: "Curry Paste",
-    price: "$2.02",
-    country: "Brazil",
-    image: "curry-paste.jpg"
-  },
-  {
-    id: 36,
-    name: "Avocado Oil",
-    price: "$4.45",
-    country: "Canada",
-    image: "avocado-oil.jpg"
-  },
-  {
-    id: 37,
-    name: "Queso Fresco",
-    price: "$9.24",
-    country: "Canada",
-    image: "queso-fresco.jpg"
-  },
-  {
-    id: 38,
-    name: "Key Lime Pie",
-    price: "$5.32",
-    country: "Mexico",
-    image: "key-lime-pie.jpg"
-  },
-  {
-    id: 39,
-    name: "Granny Smith Apple",
-    price: "$2.12",
-    country: "United States",
-    image: "granny-smith.jpg"
-  },
-  {
-    id: 40,
-    name: "Morning Glory Muffin Mix",
-    price: "$9.22",
-    country: "Brazil",
-    image: "muffin-mix.jpg"
-  },
-  {
-    id: 41,
-    name: "Heavy Whipping Cream",
-    price: "$8.78",
-    country: "Brazil",
-    image: 'heavy-cream.jpg'
-  },
-  {
-    id: 42,
-    name: "Red Bell Pepper",
-    price: "$9.03",
-    country: "Brazil",
-    image: 'red-bell-pepper.jpg'
-  },
-  {
-    id: 43,
-    name: "Perrier",
-    price: "$9.36",
-    country: "United States",
-    image: 'perrier.jpg'
-  },
-  {
-    id: 44,
-    name: "Yukon Gold Potatoes",
-    price: "$8.76",
-    country: "Mexico",
-    image: 'yukon-potatoes.jpg'
-  },
-  {
-    id: 45,
-    name: "Salmon Fillets",
-    price: "$6.21",
-    country: "United States",
-    image: 'salmon-fillets.jpg'
-  },
-  {
-    id: 46,
-    name: "Tilapia Fillets",
-    price: "$4.37",
-    country: "Mexico",
-    image: 'tilapia.jpg'
-  },
-  {
-    id: 47,
-    name: "Tomato Pesto",
-    price: "$9.70",
-    country: "Brazil",
-    image: 'tomato-pesto.jpg'
-  },
-  {
-    id: 48,
-    name: "Oil - Olive Bertolli",
-    price: "$8.92",
-    country: "Brazil",
-    image: 'olive-oil.jpg'
-  },
-  {
-    id: 49,
-    name: "Tear Drop Yellow Tomatoes",
-    price: "$3.32",
-    country: "Mexico",
-    image: 'yellow-tomatoes.jpg'
-  },
-  {
-    id: 50,
-    name: "Smoked Paprika",
-    price: "$8.31",
-    country: "Brazil",
-    image: 'smoked-paprika.jpg'
-  }
-]
+  "id": 1,
+  "title": "Eternal Sunshine of the Spotless Mind",
+  "description": "encoding",
+  "genre": "Drama|Romance|Sci-Fi",
+  "person": "Berri Fellman",
+  "review": 0,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 2,
+  "title": "Miss Farkku-Suomi",
+  "description": "context-sensitive",
+  "genre": "Drama|Romance",
+  "person": "Marys Mattiassi",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 3,
+  "title": "Chance",
+  "description": "strategy",
+  "genre": "Comedy|Drama",
+  "person": "Elisabet Yanyshev",
+  "review": 4,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 4,
+  "title": "Twelve",
+  "description": "Seamless",
+  "genre": "Crime|Drama|Thriller",
+  "person": "Zack Moreman",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 5,
+  "title": "Focus",
+  "description": "responsive",
+  "genre": "Drama",
+  "person": "Herve Follows",
+  "review": 3,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 6,
+  "title": "Daughters of Dolma",
+  "description": "analyzing",
+  "genre": "Documentary",
+  "person": "Cindee Fourman",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 7,
+  "title": "20 Feet from Stardom (Twenty Feet from Stardom)",
+  "description": "functionalities",
+  "genre": "Documentary",
+  "person": "Annamaria Gladden",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 8,
+  "title": "Clifford",
+  "description": "even-keeled",
+  "genre": "Comedy",
+  "person": "Stevy Griss",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 9,
+  "title": "Forget me not",
+  "description": "secured line",
+  "genre": "Documentary",
+  "person": "Cyrillus Allnutt",
+  "review": 4,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 10,
+  "title": "Front, The",
+  "description": "success",
+  "genre": "Comedy|Drama",
+  "person": "Emlyn Houten",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 11,
+  "title": "Mr. North",
+  "description": "impactful",
+  "genre": "Comedy|Drama",
+  "person": "Sloan Cheasman",
+  "review": 5,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 12,
+  "title": "The Face of Marble",
+  "description": "middleware",
+  "genre": "Horror",
+  "person": "Leonelle Wand",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 13,
+  "title": "Anything Goes",
+  "description": "Synchronised",
+  "genre": "Musical",
+  "person": "Freedman Phillcox",
+  "review": 0,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 14,
+  "title": "Mac and Me",
+  "description": "complexity",
+  "genre": "Children|Fantasy|Sci-Fi",
+  "person": "Dacy Fincher",
+  "review": 4,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 15,
+  "title": "Outbreak",
+  "description": "forecast",
+  "genre": "Action|Drama|Sci-Fi|Thriller",
+  "person": "Delmor Thirwell",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 16,
+  "title": "Hercules in the Underworld",
+  "description": "adapter",
+  "genre": "Action|Adventure|Fantasy|Sci-Fi",
+  "person": "Elizabeth Janous",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 17,
+  "title": "Aristocats, The",
+  "description": "national",
+  "genre": "Animation|Children",
+  "person": "Marci Riddle",
+  "review": 4,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 18,
+  "title": "Hamlet",
+  "description": "Secured",
+  "genre": "Drama",
+  "person": "Gery Roubert",
+  "review": 4,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 19,
+  "title": "Polly of the Circus",
+  "description": "Optimized",
+  "genre": "Comedy|Drama|Fantasy",
+  "person": "Tessie Ales0",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 20,
+  "title": "Hypocrites",
+  "description": "project",
+  "genre": "Drama|Fantasy",
+  "person": "Reagen Averill",
+  "review": 3,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 21,
+  "title": "Four Adventures of Reinette and Mirabelle (4 aventures de Reinette et Mirabelle)",
+  "description": "Graphic Interface",
+  "genre": "Comedy|Drama|Romance",
+  "person": "Athena Hestrop",
+  "review": 3,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 22,
+  "title": "Night Flier",
+  "description": "systemic",
+  "genre": "Horror",
+  "person": "Paten Milthorpe",
+  "review": 4,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 23,
+  "title": "Seventh Sign, The",
+  "description": "budgetary management",
+  "genre": "Drama|Fantasy|Thriller",
+  "person": "Dav Furminger",
+  "review": 3,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 24,
+  "title": "Kevin Hart: Seriously Funny",
+  "description": "Extended",
+  "genre": "Comedy",
+  "person": "Rebecka Danzig",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 25,
+  "title": "Brother Rat",
+  "description": "non-volatile",
+  "genre": "Comedy",
+  "person": "Boot Poundsford",
+  "review": 0,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 26,
+  "title": "Romeo and Juliet",
+  "description": "Balanced",
+  "genre": "Drama|Romance",
+  "person": "Frank Miners",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 27,
+  "title": "Border Radio",
+  "description": "Triple-buffered",
+  "genre": "Drama",
+  "person": "Aubrey Kelsell",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 28,
+  "title": "Burn",
+  "description": "project",
+  "genre": "Documentary",
+  "person": "Jordan Wadeling",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 29,
+  "title": "Tommy",
+  "description": "maximized",
+  "genre": "Drama|Musical",
+  "person": "Llywellyn Scholar",
+  "review": 5,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 30,
+  "title": "Pathology",
+  "description": "Ergonomic",
+  "genre": "Crime|Horror|Thriller",
+  "person": "Chelsey FitzGilbert",
+  "review": 5,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 31,
+  "title": "Sure Thing, The",
+  "description": "context-sensitive",
+  "genre": "Comedy|Romance",
+  "person": "Ailene Shorie",
+  "review": 3,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 32,
+  "title": "Wonder Boys",
+  "description": "Intuitive",
+  "genre": "Comedy|Drama",
+  "person": "Carmelita Fearnall",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 33,
+  "title": "Peg o' My Heart",
+  "description": "well-modulated",
+  "genre": "Drama|Romance",
+  "person": "Christabella Swinford",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 34,
+  "title": "Scanners III: The Takeover (Scanner Force)",
+  "description": "executive",
+  "genre": "Action|Horror|Sci-Fi",
+  "person": "Allene O'dell",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 35,
+  "title": "Clay Bird, The (Matir moina)",
+  "description": "zero defect",
+  "genre": "Drama",
+  "person": "Francine Unwins",
+  "review": 0,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 36,
+  "title": "It Should Happen to You",
+  "description": "concept",
+  "genre": "Comedy|Romance",
+  "person": "Kendal Biddwell",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 37,
+  "title": "Captain America: The First Avenger",
+  "description": "synergy",
+  "genre": "Action|Adventure|Sci-Fi|Thriller|War",
+  "person": "Doria Saltmarsh",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 38,
+  "title": "Rise of the Zombies",
+  "description": "mobile",
+  "genre": "Action|Horror|Thriller",
+  "person": "Kirby Quartermain",
+  "review": 0,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 39,
+  "title": "Arizona",
+  "description": "tangible",
+  "genre": "Western",
+  "person": "Wanda McGeady",
+  "review": 4,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 40,
+  "title": "Blue Umbrella, The",
+  "description": "info-mediaries",
+  "genre": "Animation",
+  "person": "Brianna Cragell",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+}, {
+  "id": 41,
+  "title": "Unspeakable Act, The",
+  "description": "mission-critical",
+  "genre": "Drama",
+  "person": "Lazaro Buick",
+  "review": 0,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 42,
+  "title": "Thief, The (Vor)",
+  "description": "Automated",
+  "genre": "Drama",
+  "person": "Avie Musico",
+  "review": 0,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 43,
+  "title": "Germany Year Zero (Germania anno zero) (Deutschland im Jahre Null)",
+  "description": "bottom-line",
+  "genre": "Drama|War",
+  "person": "Elna Bassingden",
+  "review": 3,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 44,
+  "title": "American Addict",
+  "description": "Networked",
+  "genre": "Documentary",
+  "person": "Becka Rankcom",
+  "review": 4,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 45,
+  "title": "The Search for Santa Paws",
+  "description": "bottom-line",
+  "genre": "Adventure|Children",
+  "person": "Rubin Sugg",
+  "review": 5,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 46,
+  "title": "Five Fingers",
+  "description": "collaboration",
+  "genre": "Drama|Thriller",
+  "person": "Phaedra Revelle",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+}, {
+  "id": 47,
+  "title": "5 Days of War",
+  "description": "Versatile",
+  "genre": "Action|Drama|War",
+  "person": "Judon Paramore",
+  "review": 3,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 48,
+  "title": "Bottom of the Sea, The (El fondo del mar)",
+  "description": "Multi-lateral",
+  "genre": "Drama|Thriller",
+  "person": "Frederigo Sporner",
+  "review": 2,
+  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+}, {
+  "id": 49,
+  "title": "Left Behind: The Movie",
+  "description": "multi-state",
+  "genre": "Action|Adventure|Drama|Thriller",
+  "person": "Annecorinne Skyner",
+  "review": 1,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}, {
+  "id": 50,
+  "title": "Fly Me to the Moon",
+  "description": "ability",
+  "genre": "Adventure|Animation|Children|IMAX",
+  "person": "Harwilll Simpkiss",
+  "review": 5,
+  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+}]
 
 export default mock;
