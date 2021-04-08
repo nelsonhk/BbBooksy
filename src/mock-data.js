16c57ce259a9d541b14e651876bc1aec230eda4e
Kimberly McKinnon <kimberlite17@gmail.com>
2021-04-07 19:19:54 -0600

    changed details

--- src/mock-data.js
+++ src/mock-data.js
@@ -4,400 +4,400 @@ let mock = [{
   "description": "encoding",
   "genre": "Drama|Romance|Sci-Fi",
   "person": "Berri Fellman",
-  "review": 0,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 0,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 2,
   "title": "Miss Farkku-Suomi",
   "description": "context-sensitive",
   "genre": "Drama|Romance",
   "person": "Marys Mattiassi",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 3,
   "title": "Chance",
   "description": "strategy",
   "genre": "Comedy|Drama",
   "person": "Elisabet Yanyshev",
-  "review": 4,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 4,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 4,
   "title": "Twelve",
   "description": "Seamless",
   "genre": "Crime|Drama|Thriller",
   "person": "Zack Moreman",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 5,
   "title": "Focus",
   "description": "responsive",
   "genre": "Drama",
   "person": "Herve Follows",
-  "review": 3,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 3,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 6,
   "title": "Daughters of Dolma",
   "description": "analyzing",
   "genre": "Documentary",
   "person": "Cindee Fourman",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 7,
   "title": "20 Feet from Stardom (Twenty Feet from Stardom)",
   "description": "functionalities",
   "genre": "Documentary",
   "person": "Annamaria Gladden",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 8,
   "title": "Clifford",
   "description": "even-keeled",
   "genre": "Comedy",
   "person": "Stevy Griss",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 9,
   "title": "Forget me not",
   "description": "secured line",
   "genre": "Documentary",
   "person": "Cyrillus Allnutt",
-  "review": 4,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 4,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 10,
   "title": "Front, The",
   "description": "success",
   "genre": "Comedy|Drama",
   "person": "Emlyn Houten",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 11,
   "title": "Mr. North",
   "description": "impactful",
   "genre": "Comedy|Drama",
   "person": "Sloan Cheasman",
-  "review": 5,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 5,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 12,
   "title": "The Face of Marble",
   "description": "middleware",
   "genre": "Horror",
   "person": "Leonelle Wand",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 13,
   "title": "Anything Goes",
   "description": "Synchronised",
   "genre": "Musical",
   "person": "Freedman Phillcox",
-  "review": 0,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 0,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 14,
   "title": "Mac and Me",
   "description": "complexity",
   "genre": "Children|Fantasy|Sci-Fi",
   "person": "Dacy Fincher",
-  "review": 4,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 4,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 15,
   "title": "Outbreak",
   "description": "forecast",
   "genre": "Action|Drama|Sci-Fi|Thriller",
   "person": "Delmor Thirwell",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 16,
   "title": "Hercules in the Underworld",
   "description": "adapter",
   "genre": "Action|Adventure|Fantasy|Sci-Fi",
   "person": "Elizabeth Janous",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 17,
   "title": "Aristocats, The",
   "description": "national",
   "genre": "Animation|Children",
   "person": "Marci Riddle",
-  "review": 4,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 4,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 18,
   "title": "Hamlet",
   "description": "Secured",
   "genre": "Drama",
   "person": "Gery Roubert",
-  "review": 4,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 4,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 19,
   "title": "Polly of the Circus",
   "description": "Optimized",
   "genre": "Comedy|Drama|Fantasy",
   "person": "Tessie Ales0",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 20,
   "title": "Hypocrites",
   "description": "project",
   "genre": "Drama|Fantasy",
   "person": "Reagen Averill",
-  "review": 3,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 3,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 21,
   "title": "Four Adventures of Reinette and Mirabelle (4 aventures de Reinette et Mirabelle)",
   "description": "Graphic Interface",
   "genre": "Comedy|Drama|Romance",
   "person": "Athena Hestrop",
-  "review": 3,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 3,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 22,
   "title": "Night Flier",
   "description": "systemic",
   "genre": "Horror",
   "person": "Paten Milthorpe",
-  "review": 4,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 4,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 23,
   "title": "Seventh Sign, The",
   "description": "budgetary management",
   "genre": "Drama|Fantasy|Thriller",
   "person": "Dav Furminger",
-  "review": 3,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 3,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 24,
   "title": "Kevin Hart: Seriously Funny",
   "description": "Extended",
   "genre": "Comedy",
   "person": "Rebecka Danzig",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 25,
   "title": "Brother Rat",
   "description": "non-volatile",
   "genre": "Comedy",
   "person": "Boot Poundsford",
-  "review": 0,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 0,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 26,
   "title": "Romeo and Juliet",
   "description": "Balanced",
   "genre": "Drama|Romance",
   "person": "Frank Miners",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 27,
   "title": "Border Radio",
   "description": "Triple-buffered",
   "genre": "Drama",
   "person": "Aubrey Kelsell",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 28,
   "title": "Burn",
   "description": "project",
   "genre": "Documentary",
   "person": "Jordan Wadeling",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 29,
   "title": "Tommy",
   "description": "maximized",
   "genre": "Drama|Musical",
   "person": "Llywellyn Scholar",
-  "review": 5,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 5,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 30,
   "title": "Pathology",
   "description": "Ergonomic",
   "genre": "Crime|Horror|Thriller",
   "person": "Chelsey FitzGilbert",
-  "review": 5,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 5,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 31,
   "title": "Sure Thing, The",
   "description": "context-sensitive",
   "genre": "Comedy|Romance",
   "person": "Ailene Shorie",
-  "review": 3,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 3,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 32,
   "title": "Wonder Boys",
   "description": "Intuitive",
   "genre": "Comedy|Drama",
   "person": "Carmelita Fearnall",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 33,
   "title": "Peg o' My Heart",
   "description": "well-modulated",
   "genre": "Drama|Romance",
   "person": "Christabella Swinford",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 34,
   "title": "Scanners III: The Takeover (Scanner Force)",
   "description": "executive",
   "genre": "Action|Horror|Sci-Fi",
   "person": "Allene O'dell",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 35,
   "title": "Clay Bird, The (Matir moina)",
   "description": "zero defect",
   "genre": "Drama",
   "person": "Francine Unwins",
-  "review": 0,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 0,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 36,
   "title": "It Should Happen to You",
   "description": "concept",
   "genre": "Comedy|Romance",
   "person": "Kendal Biddwell",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 37,
   "title": "Captain America: The First Avenger",
   "description": "synergy",
   "genre": "Action|Adventure|Sci-Fi|Thriller|War",
   "person": "Doria Saltmarsh",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 38,
   "title": "Rise of the Zombies",
   "description": "mobile",
   "genre": "Action|Horror|Thriller",
   "person": "Kirby Quartermain",
-  "review": 0,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 0,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 39,
   "title": "Arizona",
   "description": "tangible",
   "genre": "Western",
   "person": "Wanda McGeady",
-  "review": 4,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 4,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 40,
   "title": "Blue Umbrella, The",
   "description": "info-mediaries",
   "genre": "Animation",
   "person": "Brianna Cragell",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/5fa2dd/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 41,
   "title": "Unspeakable Act, The",
   "description": "mission-critical",
   "genre": "Drama",
   "person": "Lazaro Buick",
-  "review": 0,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 0,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 42,
   "title": "Thief, The (Vor)",
   "description": "Automated",
   "genre": "Drama",
   "person": "Avie Musico",
-  "review": 0,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 0,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 43,
   "title": "Germany Year Zero (Germania anno zero) (Deutschland im Jahre Null)",
   "description": "bottom-line",
   "genre": "Drama|War",
   "person": "Elna Bassingden",
-  "review": 3,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 3,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 44,
   "title": "American Addict",
   "description": "Networked",
   "genre": "Documentary",
   "person": "Becka Rankcom",
-  "review": 4,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 4,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 45,
   "title": "The Search for Santa Paws",
   "description": "bottom-line",
   "genre": "Adventure|Children",
   "person": "Rubin Sugg",
-  "review": 5,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 5,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 46,
   "title": "Five Fingers",
   "description": "collaboration",
   "genre": "Drama|Thriller",
   "person": "Phaedra Revelle",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/ff4444/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 47,
   "title": "5 Days of War",
   "description": "Versatile",
   "genre": "Action|Drama|War",
   "person": "Judon Paramore",
-  "review": 3,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 3,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 48,
   "title": "Bottom of the Sea, The (El fondo del mar)",
   "description": "Multi-lateral",
   "genre": "Drama|Thriller",
   "person": "Frederigo Sporner",
-  "review": 2,
-  "cover": "http://dummyimage.com/100x100.png/dddddd/000000"
+  "rating": 2,
+  "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 49,
   "title": "Left Behind: The Movie",
   "description": "multi-state",
   "genre": "Action|Adventure|Drama|Thriller",
   "person": "Annecorinne Skyner",
-  "review": 1,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 1,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 50,
   "title": "Fly Me to the Moon",
   "description": "ability",
   "genre": "Adventure|Animation|Children|IMAX",
   "person": "Harwilll Simpkiss",
-  "review": 5,
-  "cover": "http://dummyimage.com/100x100.png/cc0000/ffffff"
+  "rating": 5,
+  "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }]
 
 export default mock;
