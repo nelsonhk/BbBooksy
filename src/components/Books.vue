1233cd31a550275eb1d48b3de65d6681d5e0032b
Kimberly McKinnon <kimberlite17@gmail.com>
2021-04-09 00:09:54 -0600

    changed books css

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -10,19 +10,19 @@
           <p>By: {{book.person}}</p>
         </div>
         <!-- <p>Description: {{book.description}}</p> -->
-      </div>
-      <div class="cover">
-        <img :src="book.cover"/>
-      </div>
-      <div class="info-item"> 
-          <p>Genre: {{book.genre}}</p>
         </div>
-      <div class="info2">
-        <div class="info-item"> 
-          <p>Average Rating: {{book.rating}}</p>
+        <div class="cover">
+            <img :src="book.cover" width=100%/>
         </div>
         <div class="info-item"> 
-          <p>What critics are saying: {{book.review}}</p>
+            <p><em>Genre: </em>{{book.genre}}</p>
+        </div>
+        <div class="info2">
+        <div class="info-item"> 
+          <p><em>Average Rating: </em>{{book.rating}}</p>
+        </div>
+        <div class="info-item"> 
+          <p><em>What critics are saying:</em> {{book.review}}</p>
         </div>
       </div>
     </div>
@@ -56,24 +56,31 @@ export default {
 }
 .cover {
   width: 70%;
-  margin: auto;
+  margin: 15px auto;
 }
 
 .info1 {
-  padding-bottom: 6%;
+  /* padding-bottom: 6%; */
+  border: dashed 2px #a13b0c;
+  /* margin: 15px 15px; */
 }
-
+.info-item1 {
+  text-align: center;
+  margin: 15px;
+  /* margin-bottom: 5px; */
+}
+/* 
 .info2 {
   padding-top: 6%;
-}
+} */
 
 .info-item {
-  padding-top: 1%;
+    /* border: 2px solid black; */
+  line-height: 1.25em;
+  padding-top: 1.5%;
   padding-bottom: 1%;
 }
 
-.info-item1 {
-  text-align: center;
-}
+
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -107,19 +107,19 @@ body {
 }
 
 /* Masonry on large screens */
-@media only screen and (min-width: 1024px) {
+@media only screen and (min-width: 1400px) {
   .image-gallery {
     column-count: 4;
   }
 }
 /* Masonry on medium-sized screens */
-@media only screen and (max-width: 1023px) and (min-width: 768px) {
+@media only screen and (max-width: 1399px) and (min-width: 1000px) {
   .image-gallery {
     column-count: 3;
   }
 }
 /* Masonry on small screens */
-@media only screen and (max-width: 767px) and (min-width: 540px) {
+@media only screen and (max-width: 999px) and (min-width: 540px) {
   .image-gallery {
     column-count: 2;
   }
