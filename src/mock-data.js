de3085b89891beb160b0f5416d94742a597548b1
Kimberly McKinnon <kimberlite17@gmail.com>
2021-04-08 22:42:19 -0600

    added reviews to the mock-data

--- src/mock-data.js
+++ src/mock-data.js
@@ -5,6 +5,7 @@ let mock = [{
   "genre": "Drama|Romance|Sci-Fi",
   "person": "Berri Fellman",
   "rating": 0,
+  "review": "Such a fun light read",
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 2,
@@ -13,6 +14,7 @@ let mock = [{
   "genre": "Drama|Romance",
   "person": "Marys Mattiassi",
   "rating": 2,
+  "review": "A drama well suited for our times", 
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 3,
@@ -21,6 +23,7 @@ let mock = [{
   "genre": "Comedy|Drama",
   "person": "Elisabet Yanyshev",
   "rating": 4,
+  "review": "A hilarious and strategic game of romance, I'm so glad they got together in the end",
   "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 4,
@@ -29,6 +32,7 @@ let mock = [{
   "genre": "Crime|Drama|Thriller",
   "person": "Zack Moreman",
   "rating": 2,
+  "review": "What a riviting retelling of the twelve dancing princesses!", 
   "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 5,
@@ -37,6 +41,7 @@ let mock = [{
   "genre": "Drama",
   "person": "Herve Follows",
   "rating": 3,
+  "review": "worst film I've seen all year", 
   "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 6,
@@ -45,6 +50,7 @@ let mock = [{
   "genre": "Documentary",
   "person": "Cindee Fourman",
   "rating": 1,
+  "review": "the storyline was terrible, but the camera work was nice", 
   "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 7,
@@ -53,6 +59,7 @@ let mock = [{
   "genre": "Documentary",
   "person": "Annamaria Gladden",
   "rating": 2,
+  "review": "Very disappointed - absolutely no stars were in this piece. Just people.", 
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 8,
@@ -61,6 +68,7 @@ let mock = [{
   "genre": "Comedy",
   "person": "Stevy Griss",
   "rating": 1,
+  "review": "", 
   "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 9,
@@ -69,6 +77,7 @@ let mock = [{
   "genre": "Documentary",
   "person": "Cyrillus Allnutt",
   "rating": 4,
+  "review": "So bad I already forgot it", 
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 10,
@@ -77,6 +86,7 @@ let mock = [{
   "genre": "Comedy|Drama",
   "person": "Emlyn Houten",
   "rating": 1,
+  "review": "this had me laughing the entire time", 
   "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 11,
@@ -85,6 +95,7 @@ let mock = [{
   "genre": "Comedy|Drama",
   "person": "Sloan Cheasman",
   "rating": 5,
+  "review": "such mixed messages - was is supposed to be a comedy?", 
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 12,
@@ -93,6 +104,7 @@ let mock = [{
   "genre": "Horror",
   "person": "Leonelle Wand",
   "rating": 1,
+  "review": "", 
   "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 13,
@@ -101,6 +113,7 @@ let mock = [{
   "genre": "Musical",
   "person": "Freedman Phillcox",
   "rating": 0,
+  "review": "Contrary to the title, not everything goes. This book sucks.", 
   "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 14,
@@ -109,6 +122,7 @@ let mock = [{
   "genre": "Children|Fantasy|Sci-Fi",
   "person": "Dacy Fincher",
   "rating": 4,
+  "review": "A touching read about mac&n cheese with space robots. My favorite books.", 
   "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 15,
@@ -117,6 +131,7 @@ let mock = [{
   "genre": "Action|Drama|Sci-Fi|Thriller",
   "person": "Delmor Thirwell",
   "rating": 1,
+  "review": "An action novel about corona seems strange since everyone was in quarantine", 
   "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 16,
@@ -125,6 +140,7 @@ let mock = [{
   "genre": "Action|Adventure|Fantasy|Sci-Fi",
   "person": "Elizabeth Janous",
   "rating": 2,
+  "review": "This was an ambious mix of mythology and technology and I can confidently say it sucked.", 
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 17,
@@ -133,6 +149,7 @@ let mock = [{
   "genre": "Animation|Children",
   "person": "Marci Riddle",
   "rating": 4,
+  "review": "Loosely based on the movie, a real fun picture book for children",
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 18,
@@ -141,6 +158,7 @@ let mock = [{
   "genre": "Drama",
   "person": "Gery Roubert",
   "rating": 4,
+  "review": "y so much angst", 
   "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 19,
@@ -149,6 +167,7 @@ let mock = [{
   "genre": "Comedy|Drama|Fantasy",
   "person": "Tessie Ales0",
   "rating": 1,
+  "review": "After reading this, I want to go join a circus!", 
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 20,
@@ -157,6 +176,7 @@ let mock = [{
   "genre": "Drama|Fantasy",
   "person": "Reagen Averill",
   "rating": 3,
+  "review": "The plot and side characters are amazing, but the main character is a hypocritical jerk.", 
   "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 21,
@@ -165,6 +185,7 @@ let mock = [{
   "genre": "Comedy|Drama|Romance",
   "person": "Athena Hestrop",
   "rating": 3,
+  "review": "I feel like they could have stopped after three adventures.", 
   "cover": "http://dummyimage.com/150x230.png/cc0000/ffffff"
 }, {
   "id": 22,
@@ -173,6 +194,7 @@ let mock = [{
   "genre": "Horror",
   "person": "Paten Milthorpe",
   "rating": 4,
+  "review": "This kept me up all night - first reading it, then the nightmares", 
   "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 23,
@@ -181,6 +203,7 @@ let mock = [{
   "genre": "Drama|Fantasy|Thriller",
   "person": "Dav Furminger",
   "rating": 3,
+  "review": "I'm still confused at what all the signs meant, but neat read", 
   "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }, {
   "id": 24,
@@ -189,6 +212,7 @@ let mock = [{
   "genre": "Comedy",
   "person": "Rebecka Danzig",
   "rating": 2,
+  "review": "No. Kevin Hart is not seriously funny.", 
   "cover": "http://dummyimage.com/150x230.png/5fa2dd/ffffff"
 }, {
   "id": 25,
@@ -197,6 +221,7 @@ let mock = [{
   "genre": "Comedy",
   "person": "Boot Poundsford",
   "rating": 0,
+  "review": "haha this was the worst thing I've ever laid eyes on", 
   "cover": "http://dummyimage.com/150x230.png/ff4444/ffffff"
 }, {
   "id": 26,
@@ -205,6 +230,7 @@ let mock = [{
   "genre": "Drama|Romance",
   "person": "Frank Miners",
   "rating": 1,
+  "review": "Truely a classic, but please stop making people read it in hs", 
   "cover": "http://dummyimage.com/150x230.png/dddddd/000000"
 }]
 
